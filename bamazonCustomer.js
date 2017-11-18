e96dbbdc05d8475ca2ddb1ba4dc0dc45ba3f0592
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-18 12:37:25 -0500

    checkout function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,37 +8,6 @@ var connection = mysql.createConnection({ host: 'localhost', port: 3306, user: '
 //Log all data from products table
 connection.connect(connected);
 
-//Prompt
-inquirer.prompt([
-    { type: 'input', message: 'Product ID?', name: 'id'},
-    { type: 'input', message: 'Desired quantity?', name: 'qty'}
-]).then(response => {
-    //Parse the integers from the user input
-    var newID = parseInt(response.id);
-    var newQty = parseInt(response.qty);
-
-    //Verify that the user entered a valid product ID and quantity
-    if (newID > 0 && newQty > 0) {
-
-        //Select all data for that product
-        var query = 'SELECT * FROM products WHERE ID = ?';
-        connection.query(query, [newID], (err, res) => {
-
-            //If there is enough of the product in stock for this order...
-            if(newQty <= res[0].Stock) {
-                //Update the database with the requested quantity subtracted, the order is approved
-                var newStock = res[0].Stock - newQty;
-                updateStock(res[0].ID, newStock);
-                //Log the order details and total price
-                console.log(newQty, res[0].Product + '(s)', 'successfully ordered! Your total is ' + '$' + (newQty * res[0].Price).toFixed(2));
-            }
-            else console.log('Insufficient quantity.');
-        });
-    }
-    else console.log('Invalid ID.');
-});
-
-
 //connected(err): runs a query that selects all data from products table, loops through the res array and logs each object's properties to the console
 function connected(err) {
     if (err) throw err;
@@ -46,12 +15,47 @@ function connected(err) {
     connection.query("SELECT * from products", (err, res) => {
         if (err) throw err;
 
-        console.log("\n", "------------------------BAMAZON!------------------------", "\n");
+        console.log("------------------------BAMAZON!------------------------");
         console.log("ID" + "\t" + "Product" + "\t\t" + "Price" + "\t" + "Department" + "\t" + "Stock", "\n");
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].ID + "\t" + res[i].Product + "\t" + res[i].Price + "\t" + res[i].Department + "\t" + res[i].Stock);
         }
         console.log("--------------------------------------------------------");
+
+        //Prompt the user after logging the products
+        checkout(res);
+    });
+}
+
+//Prompt
+function checkout(res) {
+    inquirer.prompt([
+        { type: 'input', message: 'Product ID?', name: 'id'},
+        { type: 'input', message: 'Desired quantity?', name: 'qty'}
+    ]).then(response => {
+        //Parse the integers from the user input
+        var newID = parseInt(response.id);
+        var newQty = parseInt(response.qty);
+
+        //Verify that the user entered a valid product ID and quantity
+        if (newID > 0 && newQty > 0) {
+
+            //Select all data for that product
+            var query = 'SELECT * FROM products WHERE ID = ?';
+            connection.query(query, [newID], (err, res) => {
+
+                //If there is enough of the product in stock for this order...
+                if(newQty <= res[0].Stock) {
+                    //Update the database with the requested quantity subtracted, the order is approved
+                    var newStock = res[0].Stock - newQty;
+                    updateStock(res[0].ID, newStock);
+                    //Log the order details and total price
+                    console.log(newQty, res[0].Product + '(s)', 'successfully ordered! Your total is ' + '$' + (newQty * res[0].Price).toFixed(2));
+                }
+                else console.log('Insufficient quantity.');
+            });
+        }
+        else console.log('Invalid ID.');
     });
 }
 
