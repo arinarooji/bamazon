6b94e8d43461ff587d186cc51c37909aa89e79b5
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-17 22:26:52 -0500

    JS: updates database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,48 +5,60 @@ var inquirer = require('inquirer');
 //Connect to Bamazon
 var connection = mysql.createConnection({ host: 'localhost', port: 3306, user: 'root', password: '8283263ka', database: 'bamazon' });
 
-var questions = [{
-    type: 'input',
-    message: 'Please enter the ID of the product you would like to purchase.',
-    name: 'id'
-}, {
-    type: 'input',
-    message: 'Please enter the quantity of this product you would like to purchase.',
-    name: 'qty'
-}];
-
-connection.connect(connected); //Connect to the specified database and invoke this function (connected())
-
-//connected(err): runs a query that selects all data from products table. Invokes queryResults to log the results.
-function connected(err) {
-    if (err) throw err;
-    connection.query("SELECT * from products", queryResults);
-}
-
-//queryResults(err, res): Logs the column names, then loops/logs through res array which holds each object in the products table.
-function queryResults(err, res) {
-    if (err) throw err;
-    console.log("\n", "------------------------BAMAZON!------------------------", "\n");
-    console.log("ID" + "\t" + "Product" + "\t\t" + "Price" + "\t" + "Department" + "\t" + "Stock", "\n");
-    
-    for (var i = 0; i < res.length; i++) {
-        console.log(res[i].ID + "\t" + res[i].Product + "\t" + res[i].Price + "\t" + res[i].Department + "\t" + res[i].Stock);
-    }
-    console.log("--------------------------------------------------------");
-}
+//Log all data from products table
+connection.connect(connected);
 
 //Prompt
-inquirer.prompt(questions).then(response => {
-    //Query the product ID, related info from database
-    if (response.id > 0) {
-        var query = "SELECT * FROM products WHERE ID = ?";
-        connection.query(query, [response.id], function(err, res) {
-            if(response.qty > 0 && response.qty <= res[0].Stock) {
-                console.log(response.qty, res[0].Product + '(s)', 'successfully purchased. Your total is', '$'+ (response.qty * res[0].Price));
-                //Update database table
+inquirer.prompt([
+    { type: 'input', message: 'Product ID?', name: 'id'},
+    { type: 'input', message: 'Desired quantity?', name: 'qty'}
+]).then(response => {
+    //Parse the integers from the user input
+    var newID = parseInt(response.id);
+    var newQty = parseInt(response.qty);
+
+    //Verify that the user entered a valid product ID and quantity
+    if (newID > 0 && newQty > 0) {
+
+        //Select all data for that product
+        var query = 'SELECT * FROM products WHERE ID = ?';
+        connection.query(query, [newID], (err, res) => {
+
+            //If there is enough of the product in stock for this order...
+            if(newQty <= res[0].Stock) {
+                //Update the database with the requested quantity subtracted, the order is approved
+                var newStock = res[0].Stock - newQty;
+                updateStock(res[0].ID, newStock);
+                //Log the order details and total price
+                console.log(newQty, res[0].Product + '(s)', 'successfully ordered! Your total is ' + '$' + (newQty * res[0].Price).toFixed(2));
             }
             else console.log('Insufficient quantity.');
         });
     }
     else console.log('Invalid ID.');
 });
+
+
+//connected(err): runs a query that selects all data from products table, loops through the res array and logs properties to the console
+function connected(err) {
+    if (err) throw err;
+
+    connection.query("SELECT * from products", (err, res) => {
+        if (err) throw err;
+
+        console.log("\n", "------------------------BAMAZON!------------------------", "\n");
+        console.log("ID" + "\t" + "Product" + "\t\t" + "Price" + "\t" + "Department" + "\t" + "Stock", "\n");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].ID + "\t" + res[i].Product + "\t" + res[i].Price + "\t" + res[i].Department + "\t" + res[i].Stock);
+        }
+        console.log("--------------------------------------------------------");
+    });
+}
+
+//updateStock(): Updates the specified product ID's stock in the database
+function updateStock(id, stock) {
+    var query = 'UPDATE products SET ? WHERE ID =' + id;
+    connection.query(query, [{Stock: stock}], (err, results) => {
+        if (err) throw err;
+    });
+}
